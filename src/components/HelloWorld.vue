feb5a2255d769488defe0d9800f54028a1d54dbf
Nic1911 <nicolo.simioni.cfv@gmail.com>
2020-11-11 09:04:57 +0100

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="hello">
 		<h1>{{ msg }}</h1>
+		<h2>{{ desc }}</h2>
 	</div>
 </template>
 
@@ -9,6 +10,7 @@ export default {
 	name: "HelloWorld",
 	props: {
 		msg: String,
+		desc: String,
 	},
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<HelloWorld :msg="title" :desc="description" />
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// components
+import HelloWorld from "@/components/HelloWorld.vue";
+// data
+import appConfig from "@/app.config.json";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+	components: {
+		HelloWorld,
+	},
+	data() {
+		return {
+			title: appConfig.title,
+			description: appConfig.description,
+		};
+	},
+};
 </script>
